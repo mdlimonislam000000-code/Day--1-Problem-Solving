31cf8c9c28a5d94cef38818d78003b65138f081f
Md Limon Mia <mdlimonislam000000@gmail.com>
2026-09-15 12:59:38 +0600

    Problem -3 is solved

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const swap = (a , b) =>{
     a = a - b ;
     return [a , b]
 }
-console.log('Problem - 1 Swap :',swap(10 , 5))
+console.log('Problem - 1 : Swap [a,b] ',swap(10 , 5))
 
 // Problem - 2 
 
@@ -17,4 +17,17 @@ const isEven = (n) =>{
         return false
     }
 }
-console.log('Problem - 2 ',isEven(6))
+console.log('Problem - 2 :',isEven(6))
+
+// Problem -3 
+
+const largest = (a , b , c) =>{
+    if( a >= b && a >= c){
+        return a ;
+    }else if ( b >= c && b>=a) {
+        return b
+    }else{
+        return c
+    }
+}
+console.log('Problem - 3 : Largest number is',largest(5,10,3))
