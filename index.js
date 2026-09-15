a896c48276cad03bf9b79b3b853381297b236796
Md Limon Mia <mdlimonislam000000@gmail.com>
2026-09-15 12:51:46 +0600

    Problem -2 solved

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ const swap = (a , b) =>{
     a = a - b ;
     return [a , b]
 }
-console.log(swap(10 , 5))
+console.log('Problem - 1 Swap :',swap(10 , 5))
+
+// Problem - 2 
+
+const isEven = (n) =>{
+    if(n % 2 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+console.log('Problem - 2 ',isEven(6))
